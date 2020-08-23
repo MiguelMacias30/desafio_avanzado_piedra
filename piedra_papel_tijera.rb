df13c4a61ac32803a4eceeaf186f4cc7c15fd4a3
Miguel Macias <miguelfernandomacias@gmail.com>
2020-08-22 21:25:07 -0400

    end

--- piedra_papel_tijera.rb
+++ piedra_papel_tijera.rb
@@ -20,3 +20,44 @@ else
   jugador1 = gets.chomp
 
 end
+
+if jugador1 == "piedra" || jugador1 == "papel" || jugador1 == "tijera"
+  puts 'turno de jugador 2'
+  puts ''
+  puts 'piedra'
+  puts 'papel'
+  puts 'tijera'
+  puts 'salir'
+  jugador2 =gets.chomp
+end
+if jugador2 == "piedra" || jugador2 == "papel" || jugador2 == "tijera"
+  puts 'opcion  valida continue'
+else
+  puts 'opcion no valida, digite de nuevo'
+  jugador2 = gets.chomp
+end
+if jugador2 == "piedra" || jugador2 == "papel" || jugador2 == "tijera"
+  puts ''
+  puts 'vamos a jugar'
+end
+if jugador1 == jugador2
+  puts 'estan empatados'
+elsif
+  jugador1 =="piedra" && jugador2 == "tijera"
+  puts 'gana piedra'
+elsif
+  jugador1 == "piedra" && jugador2 == "papel"
+  puts 'gana papel'
+elsif
+  jugador1 == "papel" && jugador2 == "tijera"
+  puts 'gana tijeras'
+elsif
+  jugador1 == "papel" && jugador2 == "piedra"
+  puts 'gana papel'
+elsif
+  jugador1 == "tijera" && jugador2 == "papel"
+  puts "gana tijeras"
+else
+  jugador1 == "tijera" && jugador2 == "piedra"
+  puts 'gana piedra'
+end
